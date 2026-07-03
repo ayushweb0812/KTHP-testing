add6f811e43ae085022af4ec7108cc062ec196f5
unknown <vahanevivek@gmail.com>
2026-07-03 13:35:48 +0530

    meta data

--- app/robots.ts
+++ app/robots.ts
@@ -12,6 +12,6 @@ export default function robots(): MetadataRoute.Robots {
         "/book/",
       ],
     },
-    sitemap: "https://kilatheheritagepalace.vercel.app/sitemap.xml",
+    sitemap: "https://kilatheheritagepalace.com/sitemap.xml",
   };
 }
